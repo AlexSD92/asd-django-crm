ee8e81383e8c75f89e39d72a11bda034175f51a7
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-04-15 19:27:10 +0100

    URL mapping for privacy policy and terms and conditions as a test.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.render_dashboard, name='main-dashboard'),
+    path('privacy_policy.html/', views.render_privacy_policy, name='privacy_policy'),
+    path('terms_and_conditions.html/', views.render_t_and_c, name='terms_and_conditions'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,9 @@ from flask import render_template
 
 def render_dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
+def render_privacy_policy(request):
+    return render(request, 'dashboard/privacy_policy.html')
+
+def render_t_and_c(request):
+    return render(request, 'dashboard/terms_and_conditions.html')
