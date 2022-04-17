a1a02070d2f00e442df9a6ec1d7c195ce1fd1d96
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-04-17 08:02:39 +0100

    Account model created and registered. Various stylistic edits to base.html and dashboard.html.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from flask import render_template
+from .models import Account
+from django.contrib.auth.decorators import login_required
 
+accounts = [
+    {
+        'business_name': 'Commify',
+        'account_owner': 'Alex Dominguez'
+    }
+]
+
+@login_required
 def render_dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    context = {
+        'accounts': Account.objects.all()
+    }
+    return render(request, 'dashboard/dashboard.html', context)
 
 def render_privacy_policy(request):
     return render(request, 'dashboard/privacy_policy.html')
